6d4ff9f210c27eefb376089abbaa09b7595b5a8a
jboullion <jboullion83@gmail.com>
2021-10-13 16:37:48 -0500

    Tweak the download list layout

--- public/templates/single-file.php
+++ public/templates/single-file.php
@@ -21,13 +21,13 @@ the_post();
 
 <?php
 	if(! empty($fields['3d_files'])){
-		//pre_print($fields['3d_files']);
+
 		echo '<h3>'.$post->post_title.'</h3>';
 
 		echo '<div id="viewer-wrapper">
 				<babylon id="babylon-viewer"  model="'.$fields['3d_files'][0]['file']['url'].'" templates.main.params.fill-screen="true">
 					<scene debug="false" render-in-background="true" disable-camera-control="false">
-						<main-color r="0.3" g="0.3" b="0.3"></main-color>
+						<main-color r="0.5" g="0.3" b="0.3"></main-color>
 						<image-processing-configuration color-curves-enabled="true" exposure="1" contrast="1">
 							<color-curves global-hue="5">
 							</color-curves>
@@ -40,27 +40,37 @@ the_post();
 				</babylon>
 			</div>';
 
-		echo '<table>
-				<thead>
-					<tr>
-						<th>Name</th>
-						<th>Size</th>
-						<th>View</th>
-						<th>Download</th>
-					</tr>
-				</thead>
-				<tbody>';
+		// TODO: Get bootstrap or something and turn these into cards? Or some other more responsive layout
+		// echo '<table>
+		// 		<thead>
+		// 			<tr>
+		// 				<th>Name</th>
+		// 				<th>Size</th>
+		// 				<th>View</th>
+		// 				<th>Download</th>
+		// 			</tr>
+		// 		</thead>
+		// 		<tbody>';
 		
 		foreach($fields['3d_files'] as $file){
-			echo '<tr>
-					<td>'.$file['file']['title'].'</td>
-					<td class="text-center">'.Nexus_Aurora_Globals::convert_bytes( $file['file']['filesize'] ).'</td>
-					<td class="text-center"><button type="button" class="view-model" value="'.$file['file']['url'].'">View</button></td>
-					<td class="text-center"><a href="'.$file['file']['url'].'" download>Download</a></td>
-				</tr>';
+			// echo '<tr>
+			// 		<td>'.$file['file']['title'].'</td>
+			// 		<td class="text-center">'.Nexus_Aurora_Globals::convert_bytes( $file['file']['filesize'] ).'</td>
+			// 		<td class="text-center"><a href="'.$file['file']['url'].'" class="view-model" >View</a></td>
+			// 		<td class="text-center"><a href="'.$file['file']['url'].'" download>Download</a></td>
+			// 	</tr>';
+
+			echo '<div class="download-list">
+					<h4>'.$file['file']['title'].'</h4>
+					<div class="download-info">
+						<span>'.Nexus_Aurora_Globals::convert_bytes( $file['file']['filesize'] ).'</span>
+						<a href="'.$file['file']['url'].'" class="view-model" >View</a>
+						<a href="'.$file['file']['url'].'" download>Download</a>
+					</div>
+				</div>';
 		}
 
-		echo '</tbody></table>';
+		//echo '</tbody></table>';
 
 	}
 ?>
@@ -68,16 +78,12 @@ the_post();
 <script src="https://cdn.babylonjs.com/viewer/babylon.viewer.js"></script>
 <script>
 	(function($) {
-		
-		console.log('Testing');
-		
-
 		BabylonViewer.viewerManager.getViewerPromiseById('babylon-viewer').then(function (viewer) {
 			$('.view-model').click(function(e){
-				console.log('Testing Click');
-				//$('#viewer-wrapper').html('<babylon model="'+$(this).val()+'"></babylon>');
+				e.preventDefault();
+				
 				viewer.loadModel({
-					url: $(this).val()
+					url: $(this).attr('href')
 				});
 			});
 		});
