4b3d76cf13272eb9ee39086347d66a89d4fcf714
jboullion <jboullion83@gmail.com>
2021-10-17 19:05:48 -0500

    Moving to the Newsletter Plugin for subscriber list and newsletters!

--- admin/partials/settings.php
+++ admin/partials/settings.php
@@ -1,13 +1,31 @@
 <?php
 
-$error = '';
-if(! empty($_POST['nexusbot_url'])){
-  $clean_email = trim($_POST['nexusbot_url']);
-  if (filter_var($clean_email, FILTER_VALIDATE_URL)) {
-    update_option('nexusbot_url', $clean_email, false);
-    $success = "URL Updated!";
-  }else{
-    $error = "Invalid URL";
+// TODO: Update this to be a global array. Shared with class-nexus-aurora-email.php
+$settings = [
+  'urls' => [
+    'nexusbot_url' => 'Bot Url',
+    // 'facebook_url' => 'Facebook Url',
+    // 'youtube_url' => 'YouTube Url',
+    // 'twitter_url' => 'Twitter Url',
+    //'discord_url' => 'Discord Url',
+  ]
+];
+
+$successes = [];
+$errors = [];
+
+foreach($settings['urls'] as $url => $title){
+  if(! empty($_POST[$url])){
+
+    $clean_url = trim($_POST[$url]);
+
+    if (filter_var($clean_url, FILTER_VALIDATE_URL)) {
+      update_option($url, $clean_url, false);
+      $successes[] = "URL Updated!";
+    }else{
+      $errors[] = "Invalid URL";
+    }
+
   }
 }
 
@@ -23,10 +41,15 @@ $nexusbot_url = get_option('nexusbot_url');
   <form action="" method="post">
     <table class="form-table" role="presentation">
       <tbody>
-        <tr>
-          <th scope="row"><label for="nexusbot_url">Discord Bot URL</label></th>
-          <td><input name="nexusbot_url" type="url" id="nexusbot_url" value="<?php echo $nexusbot_url; ?>" class="regular-text"></td>
-        </tr>
+        <?php 
+          foreach($settings['urls'] as $url => $title){
+            $option_value = get_option($url);
+            echo '<tr>
+                    <th scope="row"><label for="'.$url.'">'.$title.'</label></th>
+                    <td><input name="'.$url.'" type="url" id="'.$url.'" value="'.$option_value.'" class="regular-text"></td>
+                  </tr>';
+          }
+        ?>
       </tbody>
     </table>
     <input name="submit" class="button button-primary" type="submit" value="<?php esc_attr_e( 'Save' ); ?>" />
--- public/class-nexus-aurora-bot-public.php
+++ public/class-nexus-aurora-bot-public.php
@@ -164,105 +164,105 @@ class Nexus_Aurora_Bot_Public {
 				</div>';
 	}
 
-	/**
-	 * Generate the subscriber form
-	 *
-	 * @param array $atts An array of attributes used to build the subscribe form
-	 * @atts[show_button] Show the form button
-	 * @return void
-	 */
-	public function na_subscribe($atts = []) {
-		global $post; 
+	// /**
+	//  * Generate the subscriber form
+	//  *
+	//  * @param array $atts An array of attributes used to build the subscribe form
+	//  * @atts[show_button] Show the form button
+	//  * @return void
+	//  */
+	// public function na_subscribe($atts = []) {
+	// 	global $post; 
 
-		if(!empty($_POST['subscribe']['email'])) $subscribe_message = $this->subscribe_submission();
+	// 	if(!empty($_POST['subscribe']['email'])) $subscribe_message = $this->subscribe_submission();
 
 		
-		return '<div id="na-subscribe-form">
+	// 	return '<div id="na-subscribe-form">
 		
-				'.( ! empty($subscribe_message['success']) ? '<div class="na-alert na-success">'.$subscribe_message['success'].'</div>':'').'
-				'.( ! empty($subscribe_message['error']) ? '<div class="na-alert na-error">'.$subscribe_message['error'].'</div>':'').'
+	// 			'.( ! empty($subscribe_message['success']) ? '<div class="na-alert na-success">'.$subscribe_message['success'].'</div>':'').'
+	// 			'.( ! empty($subscribe_message['error']) ? '<div class="na-alert na-error">'.$subscribe_message['error'].'</div>':'').'
 					
 		
-				'.( empty($subscribe_message['success']) ? '
-				<form 
-					class="na-form" 
-					method="post" 
-					name="Subscribe Form"
-					action="#na-subscribe-form">
-					<input type="hidden" name="action" value="subscribe_form">
-					<input type="hidden" name="post_id" value="'.$post->ID.'">
+	// 			'.( empty($subscribe_message['success']) ? '
+	// 			<form 
+	// 				class="na-form" 
+	// 				method="post" 
+	// 				name="Subscribe Form"
+	// 				action="#na-subscribe-form">
+	// 				<input type="hidden" name="action" value="subscribe_form">
+	// 				<input type="hidden" name="post_id" value="'.$post->ID.'">
 
-				<div class="na-input-wrapper">
-					<input size="1" type="email" name="subscribe[email]" id="subscriber-email" class="" placeholder="Enter Email" value="'.$_POST['subscribe']['email'].'">
-					'.( empty($atts['show_button']) ? '' : '<button type="submit" class="">Subscribe</button>').'
-				</div>
-			</form>
-		</div>':'');
+	// 			<div class="na-input-wrapper">
+	// 				<input size="1" type="email" name="subscribe[email]" id="subscriber-email" class="" placeholder="Enter Email" value="'.$_POST['subscribe']['email'].'">
+	// 				'.( empty($atts['show_button']) ? '' : '<button type="submit" class="">Subscribe</button>').'
+	// 			</div>
+	// 		</form>
+	// 	</div>':'');
 
-		/*
-		<div class="elementor-widget-container">
-			<form class="elementor-form" method="post" name="New Form">
-				<input type="hidden" name="post_id" value="1376">
+	// 	/*
+	// 	<div class="elementor-widget-container">
+	// 		<form class="elementor-form" method="post" name="New Form">
+	// 			<input type="hidden" name="post_id" value="1376">
 				
-				<div class="elementor-form-fields-wrapper elementor-labels-above">
-					<div class="elementor-field-type-text elementor-field-group elementor-column elementor-field-group-name elementor-col-100">
-						<input size="1" type="text" name="form_fields[name]" id="form-field-name" class="elementor-field elementor-size-sm  elementor-field-textual" placeholder="Name">
-					</div>
-				</div>
-			</form>
-		</div>
-		*/
-	}
+	// 			<div class="elementor-form-fields-wrapper elementor-labels-above">
+	// 				<div class="elementor-field-type-text elementor-field-group elementor-column elementor-field-group-name elementor-col-100">
+	// 					<input size="1" type="text" name="form_fields[name]" id="form-field-name" class="elementor-field elementor-size-sm  elementor-field-textual" placeholder="Name">
+	// 				</div>
+	// 			</div>
+	// 		</form>
+	// 	</div>
+	// 	*/
+	// }
 
-	/**
-	 * Subscribe a visitor to our newsletter
-	 *
-	 * @return void
-	 */
-	public function subscribe_submission() {
-		global $wpdb;
+	// /**
+	//  * Subscribe a visitor to our newsletter
+	//  *
+	//  * @return void
+	//  */
+	// public function subscribe_submission() {
+	// 	global $wpdb;
 		
-		if(empty($_POST['subscribe']['email'])) return;
+	// 	if(empty($_POST['subscribe']['email'])) return;
 
-		if(! filter_var($_POST['subscribe']['email'], FILTER_VALIDATE_EMAIL)) return ['error' => 'Invalid email'];
+	// 	if(! filter_var($_POST['subscribe']['email'], FILTER_VALIDATE_EMAIL)) return ['error' => 'Invalid email'];
 
-		$subscriber_email = $_POST['subscribe']['email'];
+	// 	$subscriber_email = $_POST['subscribe']['email'];
 
-		$subscriber = $wpdb->get_row($wpdb->prepare("SELECT * FROM na_subscribers WHERE email = %s", $subscriber_email));
+	// 	$subscriber = $wpdb->get_row($wpdb->prepare("SELECT * FROM na_subscribers WHERE email = %s", $subscriber_email));
 
-		if( !$subscriber ){
-			$result = $wpdb->insert( 
-				'na_subscribers', 
-				array( 
-					'email' => $subscriber_email,
-					'active' => 1,
-					'createdAt' => current_time( 'mysql' ),
-					'updatedAt' => current_time( 'mysql' ),
-				) 
-			);
+	// 	if( !$subscriber ){
+	// 		$result = $wpdb->insert( 
+	// 			'na_subscribers', 
+	// 			array( 
+	// 				'email' => $subscriber_email,
+	// 				'active' => 1,
+	// 				'createdAt' => current_time( 'mysql' ),
+	// 				'updatedAt' => current_time( 'mysql' ),
+	// 			) 
+	// 		);
 
-			if($result){
-				return ['success' => 'You are now subscribed!'];
-			}
-		}else{
-			$result = $wpdb->update( 
-				'na_subscribers', 
-				array( 
-					'active' => 1,
-					'updatedAt' => current_time( 'mysql' ),
-				),
-				array( 
-					'email' => $subscriber_email,
-				)
-			);
+	// 		if($result){
+	// 			return ['success' => 'You are now subscribed!'];
+	// 		}
+	// 	}else{
+	// 		$result = $wpdb->update( 
+	// 			'na_subscribers', 
+	// 			array( 
+	// 				'active' => 1,
+	// 				'updatedAt' => current_time( 'mysql' ),
+	// 			),
+	// 			array( 
+	// 				'email' => $subscriber_email,
+	// 			)
+	// 		);
 
-			return ['success' => 'You are now subscribed!']; // 'Already subscribed!'
+	// 		return ['success' => 'You are now subscribed!']; // 'Already subscribed!'
 
-		}
+	// 	}
 		
-		// return ['error' => 'Error: '.$wpdb->last_error];
+	// 	// return ['error' => 'Error: '.$wpdb->last_error];
 
-		return ['error' => 'Unable to subscribe'];
-	}
+	// 	return ['error' => 'Unable to subscribe'];
+	// }
 
 }
